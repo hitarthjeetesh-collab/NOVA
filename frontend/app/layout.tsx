db5a094aa3d2434f278054bcc12a32739adc73d5
Hitarth Jeetesh Jadhav <hitarthjeetesh@gmail.com>
2026-09-04 10:09:52 -0400

    hi

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -1,4 +1,5 @@
 import type { Metadata } from "next";
+import Script from "next/script";
 import "./globals.css";
 
 export const metadata: Metadata = {
@@ -13,7 +14,23 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body>
+        <Script
+          src="https://www.googletagmanager.com/gtag/js?id=G-ES46D799BM"
+          strategy="afterInteractive"
+        />
+
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){window.dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-ES46D799BM');
+          `}
+        </Script>
+
+        {children}
+      </body>
     </html>
   );
 }
